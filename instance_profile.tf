022e343f390092349ac53ae457637ca29af81f07
Valter Douglas Lisboa Junior <valter.lisboa@br.clara.net>
2017-10-03 20:11:32 -0300

    change deprecated parameter for EC2 instance profile

--- instance_profile.tf
+++ instance_profile.tf
@@ -1,8 +1,5 @@
 resource "aws_iam_instance_profile" "ec2_profile" {
   name = "${var.name}"
   path = "${var.path}"
-
-  roles = [
-    "${aws_iam_role.ec2_role.name}"
-  ]
+  role = "${aws_iam_role.ec2_role.name}"
 }
