199da92596e7fbe6f2913681054098ef56b0305e
Valter Douglas Lisboa Junior <valter.lisboa@br.clara.net>
2017-10-03 20:17:15 -0300

    fix identation

--- policies_attachments.tf
+++ policies_attachments.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_role_policy_attachment" "ec2_role_attachments" {
-    role = "${aws_iam_role.ec2_role.name}"
-    policy_arn = "${element(var.policies_arn, count.index)}"
+    role        = "${aws_iam_role.ec2_role.name}"
+    policy_arn  = "${element(var.policies_arn, count.index)}"
 
-    count = "${var.policies_count}"
+    count       = "${var.policies_count}"
 }
