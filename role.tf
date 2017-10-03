a40340338a58fc828b159f8e04b85a65e0170611
Valter Douglas Lisboa Junior <valter.lisboa@br.clara.net>
2017-10-03 20:16:28 -0300

    set role to use data source aws_iam_policy_document instead file template

--- role.tf
+++ role.tf
@@ -1,6 +1,5 @@
 resource "aws_iam_role" "ec2_role" {
-  name = "${var.name}"
-  path = "${var.path}"
-
-  assume_role_policy = "${file("${path.module}/templates/ec2-role.json")}"
+  name                = "${var.name}"
+  path                = "${var.path}"
+  assume_role_policy  = "${data.aws_iam_policy_document.ec2_role.json}"
 }
